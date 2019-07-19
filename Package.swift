70af649bf3f27bc8edb2cbdb78d6a732db88a9c9
YOCKOW <YOCKOW@users.noreply.github.com>
2019-07-19 14:42:47 +0900

    Update dependencies.

--- Package.swift
+++ Package.swift
@@ -11,11 +11,11 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url:"https://github.com/YOCKOW/SwiftBonaFideCharacterSet.git", from:"1.5.0"),
+    .package(url:"https://github.com/YOCKOW/SwiftBonaFideCharacterSet.git", from:"1.6.0"),
     .package(url:"https://github.com/YOCKOW/SwiftExtensions.git", from:"0.1.0"),
-    .package(url:"https://github.com/YOCKOW/SwiftNetworkGear.git", from:"0.8.1"),
-    .package(url:"https://github.com/YOCKOW/SwiftPredicate.git", from:"1.1.0"),
-    .package(url:"https://github.com/YOCKOW/SwiftRanges.git", from: "2.0.0"),
+    .package(url:"https://github.com/YOCKOW/SwiftNetworkGear.git", from:"0.9.0"),
+    .package(url:"https://github.com/YOCKOW/SwiftPredicate.git", from:"1.2.0"),
+    .package(url:"https://github.com/YOCKOW/SwiftRanges.git", from: "3.0.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
