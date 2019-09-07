f36cb0a2aa624630492dd13f2e42b7e6a81700c5
YOCKOW <YOCKOW@users.noreply.github.com>
2019-09-07 22:53:22 +0900

    Update dependency: ySwiftExtensions.

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     .package(url:"https://github.com/YOCKOW/SwiftBonaFideCharacterSet.git", from:"1.6.0"),
-    .package(url:"https://github.com/YOCKOW/SwiftExtensions.git", from:"0.1.0"),
     .package(url:"https://github.com/YOCKOW/SwiftNetworkGear.git", from:"0.9.0"),
     .package(url:"https://github.com/YOCKOW/SwiftPredicate.git", from:"1.2.0"),
     .package(url:"https://github.com/YOCKOW/SwiftRanges.git", from: "3.0.0"),
+    .package(url:"https://github.com/YOCKOW/ySwiftExtensions.git", from:"0.4.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,10 +23,10 @@ let package = Package(
     .target(name: "XHTML",
             dependencies: [
               "SwiftBonaFideCharacterSet",
-              "SwiftYOCKOWExtensions",
               "SwiftNetworkGear",
               "SwiftPredicate",
-              "SwiftRanges"
+              "SwiftRanges",
+              "ySwiftExtensions",
             ]),
     .target(name: "TestResources", dependencies: [], path: "Tests/TestResources"),
     .testTarget(name: "XHTMLTests",dependencies: ["XHTML", "TestResources"]),
