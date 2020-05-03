054f14fb644f641fa10c6cd4c2c4be6e94e679ec
YOCKOW <YOCKOW@users.noreply.github.com>
2020-05-03 13:26:17 +0900

    Update dependency (ySwiftExtensions): Drop support for older OS and older Swift.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "XHTML",
+  platforms: [
+    .macOS(.v10_15),
+    .iOS(.v13),
+    .watchOS(.v6),
+    .tvOS(.v13),
+  ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(name: "SwiftXHTML", type: .dynamic, targets: ["XHTML"]),
@@ -12,12 +18,11 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     .package(url:"https://github.com/YOCKOW/SwiftBonaFideCharacterSet.git", from: "1.6.2"),
-    .package(url:"https://github.com/YOCKOW/SwiftNetworkGear.git", "0.11.1"..<"2.0.0"),
-    .package(url:"https://github.com/YOCKOW/SwiftPredicate.git", from: "1.2.1"),
+    .package(url:"https://github.com/YOCKOW/SwiftNetworkGear.git", "0.13.0"..<"2.0.0"),
     .package(url:"https://github.com/YOCKOW/SwiftRanges.git", from: "3.1.1"),
-    .package(url:"https://github.com/YOCKOW/SwiftStringComposition.git", from: "1.3.1"),
+    .package(url:"https://github.com/YOCKOW/SwiftStringComposition.git", from: "2.0.0"),
     .package(url:"https://github.com/YOCKOW/SwiftUnicodeSupplement.git", from: "1.0.0"),
-    .package(url:"https://github.com/YOCKOW/ySwiftExtensions.git", "0.10.1"..<"2.0.0"),
+    .package(url:"https://github.com/YOCKOW/ySwiftExtensions.git", from: "1.0.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,7 +31,6 @@ let package = Package(
             dependencies: [
               "SwiftBonaFideCharacterSet",
               "SwiftNetworkGear",
-              "SwiftPredicate",
               "SwiftRanges",
               "SwiftStringComposition",
               "SwiftUnicodeSupplement",
@@ -35,7 +39,7 @@ let package = Package(
     .target(name: "TestResources", dependencies: [], path: "Tests/TestResources"),
     .testTarget(name: "XHTMLTests",dependencies: ["XHTML", "TestResources"]),
   ],
-  swiftLanguageVersions: [.v4, .v4_2, .v5]
+  swiftLanguageVersions: [.v5]
 )
 
 
