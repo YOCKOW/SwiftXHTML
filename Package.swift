310c71f0455872b55e885abdb727d8becc4d8809
YOCKOW <YOCKOW@users.noreply.github.com>
2020-11-17 13:31:16 +0900

    Work around for SR-13859.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "XHTML",
   platforms: [
-    .macOS(.v10_15),
+    .macOS("10.15.4"), // Workaround for https://bugs.swift.org/browse/SR-13859
     .iOS(.v13),
     .watchOS(.v6),
     .tvOS(.v13),
@@ -18,11 +18,11 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     .package(url:"https://github.com/YOCKOW/SwiftBonaFideCharacterSet.git", from: "1.6.2"),
-    .package(url:"https://github.com/YOCKOW/SwiftNetworkGear.git", "0.14.2"..<"2.0.0"),
+    .package(url:"https://github.com/YOCKOW/SwiftNetworkGear.git", "0.14.4"..<"2.0.0"),
     .package(url:"https://github.com/YOCKOW/SwiftRanges.git", from: "3.1.1"),
-    .package(url:"https://github.com/YOCKOW/SwiftStringComposition.git", from: "2.0.1"),
+    .package(url:"https://github.com/YOCKOW/SwiftStringComposition.git", from: "2.0.2"),
     .package(url:"https://github.com/YOCKOW/SwiftUnicodeSupplement.git", from: "1.0.1"),
-    .package(url:"https://github.com/YOCKOW/ySwiftExtensions.git", from: "1.5.3"),
+    .package(url:"https://github.com/YOCKOW/ySwiftExtensions.git", from: "1.6.2"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
